9ae64e2bbe96f0e8542aeb65533ee0ef62527159
IE Dev 2 <ie.dev.2@hotmail.com>
2024-09-23 09:17:54 -0700

    Parte del video terminada 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodoComponent } from "./pages/todo/todo.component";
 import { TODO_DATA } from '../assets/todo';
@@ -13,11 +13,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  todoData = TODO_DATA.filter(item=> item.id<3);
+  todoData = TODO_DATA.filter(item=> item.id<4);
 
-  constructor(){
+  // @ViewChild('todoRef', {read: ElementRef}) todo?: TodoComponent;
+  @ViewChildren('todoRef', {read: ElementRef}) todo?: QueryList<ElementRef>;
 
-  }
+  constructor(){}
 
   getTodoInfo(val: NTodo.TodoData) {
     console.log(val);
@@ -32,7 +33,14 @@ export class AppComponent {
   }
 
   selectTodo(){
-    const todo =document.querySelectorAll('app-todo');
-    console.log(todo);
+    // const todo =document.querySelectorAll('app-todo');
+    console.log(this.todo);
+    // this.todo?.changes.subscribe(values =>{
+    //   console.log(values);
+    // });
+  }
+
+  addTodo() {
+    this.todoData = TODO_DATA.filter(item=> item.id<5);
   }
 }
